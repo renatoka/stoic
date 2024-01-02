3ccdabe16e8792014b256b0ef79b5395234ca979
Renato <rkauric@vvg.hr>
2024-01-02 10:44:24 +0100

    application layout

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,25 @@
+import { View } from '@gluestack-ui/themed';
+import React, { ReactNode } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.view}>{children}</View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  view: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+});
+
+export default Layout;
