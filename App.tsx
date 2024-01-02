8b084bb044777118c2729c9386ca62e90b794283
Renato <rkauric@vvg.hr>
2024-01-02 10:43:55 +0100

    created initial layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Layout from './src/layout/Layout';
+import Home from './src/screens/home/Home';
+import { config } from '@gluestack-ui/config';
+import { Box, GluestackUIProvider, Text } from '@gluestack-ui/themed';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <Layout>
+        <Home />
+      </Layout>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
