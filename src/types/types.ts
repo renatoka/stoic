eb9208cde0706edf77fc9c2fc0bcf7dd440109c5
Renato <rkauric@vvg.hr>
2024-01-02 14:24:14 +0100

    added additional types

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,15 @@ export type Quote = {
   author: string;
   text: string;
 };
+
+export type QuoteBoxProps = {
+  quote: Quote;
+  isFullScreen: boolean;
+  toggleFullScreen: () => void;
+};
+
+export type Book = {
+  id: string;
+  title: string;
+  image: string;
+};
