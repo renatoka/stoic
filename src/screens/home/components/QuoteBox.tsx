b8ee474ee471b383276cc4c8ebc2201fa99bee40
Privatno <renatokauric@Renatos-MacBook-Air.local>
2024-01-04 16:52:56 +0100

    quote box component refactor

--- src/screens/home/components/QuoteBox.tsx
+++ src/screens/home/components/QuoteBox.tsx
@@ -1,32 +1,101 @@
-import { QuoteBoxProps } from '../../../types/types';
-import { Text, View } from '@gluestack-ui/themed';
 import { HeartIcon } from 'lucide-react-native';
-import { useRef } from 'react';
-import { Modal, Pressable, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Animated,
+  Modal,
+  PanResponder,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { MMKVLoader } from 'react-native-mmkv-storage';
 
-const QuoteBox = ({ quote, isFullScreen, setIsFullScreen }: QuoteBoxProps) => {
+const QuoteBox = ({ quote, addToFavorites, removeFromFavorites }: any) => {
+  const storage = new MMKVLoader()
+    .withInstanceID('favorites')
+    .withEncryption()
+    .initialize();
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const panY = new Animated.Value(0);
+
+  useEffect(() => {
+    const checkFavoriteStatus = async () => {
+      const favorites = await storage.getArrayAsync('favorites');
+      setIsFavorite(
+        favorites?.some((item: any) => item.id === quote.id) ?? false,
+      );
+    };
+
+    checkFavoriteStatus();
+  }, [quote.id, storage]);
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (_, gestureState) => {
+      if (gestureState.dy > 0) {
+        Animated.event([null, { dy: panY }], { useNativeDriver: false })(
+          _,
+          gestureState,
+        );
+      }
+    },
+    onPanResponderRelease: (_, gestureState) => {
+      if (gestureState.dy > 50) {
+        setModalVisible(false);
+      } else {
+        Animated.spring(panY, { toValue: 0, useNativeDriver: false }).start();
+      }
+    },
+  });
+
+  const handleHeartIconPress = async () => {
+    if (isFavorite) {
+      removeFromFavorites(quote.id);
+      setIsFavorite(false);
+      setModalVisible(false);
+    } else {
+      addToFavorites();
+      setIsFavorite(true);
+    }
+  };
+
   return (
-    <View>
-      <TouchableOpacity onPress={setIsFullScreen}>
+    <TouchableOpacity
+      onPress={() => setModalVisible(true)}
+      {...panResponder.panHandlers}
+    >
+      <View>
         <View style={styles.quoteBox}>
           <Text style={styles.quoteText}>{quote.text}</Text>
           <Text style={styles.quoteAuthor}>{quote.author}</Text>
         </View>
-      </TouchableOpacity>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={isFullScreen}
-        onRequestClose={() => {
-          setIsFullScreen(false);
-        }}
-      >
-        <View style={[styles.fullScreen, styles.fullScreenModal]}>
+      </View>
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <Animated.View
+          style={[
+            styles.fullScreen,
+            styles.fullScreenModal,
+            { transform: [{ translateY: panY }] },
+          ]}
+          {...panResponder.panHandlers}
+        >
           <Text style={styles.quoteText}>{quote.text}</Text>
           <Text style={styles.quoteAuthor}>{quote.author}</Text>
-        </View>
+          <TouchableOpacity
+            onPress={handleHeartIconPress}
+            style={styles.heartIconContainer}
+          >
+            <HeartIcon
+              size={20}
+              color="#FFFFFF"
+              fill={isFavorite ? 'red' : ''}
+            />
+          </TouchableOpacity>
+        </Animated.View>
       </Modal>
-    </View>
+    </TouchableOpacity>
   );
 };
 
@@ -35,6 +104,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#14213D',
     borderRadius: 10,
     padding: 25,
+    position: 'relative',
   },
   quoteText: {
     color: '#FFFFFF',
@@ -59,7 +129,9 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   heartIconContainer: {
-    marginTop: 20,
+    position: 'relative',
+    top: 10,
+    alignSelf: 'center',
   },
 });
 
